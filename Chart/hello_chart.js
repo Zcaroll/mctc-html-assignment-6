62f9fc093a5c7eb70c98eacf989c8e8238036e75
Zcaroll <zcarroll.bus@gmail.com>
2025-02-22 23:50:22 -0600

    Update hello_chart.js

--- Chart/hello_chart.js
+++ Chart/hello_chart.js
@@ -1,1 +1,13 @@
+let canvas = document.querySelector('#soda-chart')
+let context = canvas.getContext('2d')
 
+let chart = new Chart(context, {
+    type: 'bar',
+    data: {
+        labels: ['Coke', 'Pepsi', 'Either', 'Neither'],
+        datasets: [ {
+            label: 'Number of votes',
+            data: [18, 14, 7, 10]
+        }]
+    }
+})
