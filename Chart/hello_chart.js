5f42b35f96ea6e6f2e9da3bc67e07686c3ceb801
Zcaroll <zcarroll.bus@gmail.com>
2025-02-22 23:56:09 -0600

    Update hello_chart.js

--- Chart/hello_chart.js
+++ Chart/hello_chart.js
@@ -4,10 +4,20 @@ let context = canvas.getContext('2d')
 let chart = new Chart(context, {
     type: 'bar',
     data: {
-        labels: ['Coke', 'Pepsi', 'Either', 'Neither'],
+        labels: ['Coke', 'Pepsi', 'Sprite', 'Either', 'Neither'],
         datasets: [ {
             label: 'Number of votes',
-            data: [18, 14, 7, 10]
+            data: [18, 14, 12, 7, 10],
+            backgroundColor: ['red', 'blue', 'yellowgreen', 'green', 'grey']
         }]
+    },
+    options: {
+        scales: {
+            yAxes: [ {
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
     }
 })
