adbf082693c3f1973c9ca0ecba78ad9057980b00
Zcaroll <zcarroll.bus@gmail.com>
2025-02-25 16:43:31 -0600

    Create bridges.js

---
+++ bridges/bridges.js
@@ -0,0 +1,10 @@
+let metroAreaCenterCoordinates = [39.28, -96.8] //random near middle coordinate
+let zoomLevel = 7 //probably country level
+
+campuses =  [
+    {"name": "Verrazzano-Narrows Bridge", "location": "New York, NY", "coordinates": [, ] }, 
+    {"name": "Golden Gate Bridge", "location": "San Francisco and Marin, CA", "coordinates": [, ] }, 
+    {"name": "Mackinac Bridge", "location": "Mackinaw and St Ignace, MI", "coordinates": [, ] }, 
+    {"name": "George Washington Bridge", "location": "New York, NY and New Jersey, NJ", "coordinates": [,] }, 
+    {"name": "Tacoma Narrows Bridge", "location": "Tacoma and Kitsap, WA", "coordinates": [,] }
+]
